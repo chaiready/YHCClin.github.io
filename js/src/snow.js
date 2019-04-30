3c3093413ac374ea2a184db250e4a8f4bfd2ec1c
YHCClin <30885631+YHCClin@users.noreply.github.com>
2019-05-01 00:29:40 +0800

    Site updated: 2019-05-01 00:29:11

--- js/src/snow.js
+++ js/src/snow.js
@@ -3,8 +3,8 @@
 function snowFall(snow) {
     /* 可配置属性 */
     snow = snow || {};
-    this.maxFlake = snow.maxFlake || 200;   /* 最多片数 */
-    this.flakeSize = snow.flakeSize || 10;  /* 雪花形状 */
+    this.maxFlake = snow.maxFlake || 300;   /* 最多片数 */
+    this.flakeSize = snow.flakeSize || 15;  /* 雪花形状 */
     this.fallSpeed = snow.fallSpeed || 1;   /* 坠落速度 */
 }
 /* 兼容写法 */
@@ -88,9 +88,9 @@ flakeMove.prototype.reset = function(width, height) {
 // 渲染雪花-随机形状（此处可修改雪花颜色！！！）
 flakeMove.prototype.render = function(ctx) {
     var snowFlake = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.size);
-    snowFlake.addColorStop(0, "rgba(0, 255, 255, 0.9)");  /* 此处是雪花颜色，默认是白色 */
-    snowFlake.addColorStop(.5, "rgba(0, 255, 255, 0.5)"); /* 若要改为其他颜色，请自行查 */
-    snowFlake.addColorStop(1, "rgba(0, 255, 255, 0)");    /* 找16进制的RGB 颜色代码。 */
+    snowFlake.addColorStop(0, "rgba(238, 233, 233, 0.9)");  /* 此处是雪花颜色，默认是白色 */
+    snowFlake.addColorStop(.5, "rgba(238, 233, 233, 0.5)"); /* 若要改为其他颜色，请自行查 */
+    snowFlake.addColorStop(1, "rgba(238, 233, 233, 0)");    /* 找16进制的RGB 颜色代码。 */
     ctx.save();
     ctx.fillStyle = snowFlake;
     ctx.beginPath();
